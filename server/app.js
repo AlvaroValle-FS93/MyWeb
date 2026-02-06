1973dc1f23fe99e73ed6f20e857c7125d0fa31a6
Álvaro Valle Melgarejo <alvaro.valle.fullstack@outlook.com>
2026-02-06 18:26:14 +0100

    fix express generator beginning problems and install librarys

--- server/app.js
+++ server/app.js
@@ -1,26 +1,31 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import createError from 'http-errors';
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+dotenv.config(); 
 
-var app = express();
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+app.use(cors({
+  origin: process.env.FRONT_URL,
+  credentials: true
+}))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+/* app.use('/', indexRouter);
+app.use('/users', usersRouter); */
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,4 +43,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+export default app;
